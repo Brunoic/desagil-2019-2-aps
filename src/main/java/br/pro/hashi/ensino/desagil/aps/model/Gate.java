1aff01e0b8fd95c2ca89ba3b2303fa565dd3a2f0
Brunoic <bruno@pnl.com.br>
2019-09-10 22:40:16 -0300

    conteúdos APS1 entregues

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/Gate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/Gate.java
@@ -2,11 +2,16 @@ package br.pro.hashi.ensino.desagil.aps.model;
 
 public abstract class Gate implements SignalEmitter, SignalReceiver {
     private final int inputSize;
+    private final String name;
 
-    protected Gate(int inputSize) {
+    protected Gate(String name,int inputSize) {
+        this.name = name;
         this.inputSize = inputSize;
     }
 
+    public String toString() {
+        return name;
+    }
     public int getInputSize() {
         return inputSize;
     }
